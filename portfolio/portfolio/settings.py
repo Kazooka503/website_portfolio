d95fa005b6a68ebae1bc4bc62cd21709565218b4
Austin <austinkrz@gmail.com>
2024-04-07 18:58:39 -0700

    Debugging in process

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%y422$rt&l15qb!-wzz6po+h5d%_)t4srx1ia_1&$9mpsqj3l@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
