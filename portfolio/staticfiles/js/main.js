1fed4fef4622fb51d8236dc987e1760012f08a13
Austin <austinkrz@gmail.com>
2024-04-09 16:42:52 -0700

    Moves all JavaScript to its own file.
    Fixes rendering bug where colors wouldn't load until mouse moved.

--- portfolio/staticfiles/js/main.js
+++ portfolio/staticfiles/js/main.js
@@ -1,3 +1,15 @@
+const btns = document.getElementsByClassName("nav-link");
+    for (var i = 0; i < btns.length; i++){
+        btns[i].addEventListener("click", function(){
+            var current = document.getElementsByClassName("active");
+            var currentChildren = current[0].getElementsByTagName("hr");
+            current[0].className = current[0].className.replace(" active", "");
+            currentChildren[0].className = currentChildren[0].className.replace(" active-line", "");
+            this.className += " active";
+            this.getElementsByTagName("hr")[0].className += " active-line";
+        });
+    }
+
 const injectCursorPosition = ({ x, y }) => {
         document.documentElement.style.setProperty('--x', Math.round(x))
         document.documentElement.style.setProperty('--y', Math.round(y))
